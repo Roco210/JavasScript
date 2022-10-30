d3e9819b760307fe6389b1b6ba8f9961ed97721f
Ricardo <rickyfernandez210@gmail.com>
2022-10-30 15:13:29 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,68 @@
+
+class Producto{
+    constructor(id,nombre,precio,cantidadComprada){
+        this.id=id,
+        this.nombre=nombre,
+        this.precio=precio,
+        this.cantidadComprada=cantidadComprada
+    }
+}
+
+const maceta =new Producto(1,"maceta 5 lts", 1000,0)
+const sustrato=new Producto(2,"sustrato ligero 25dm",2200,0)
+const  fertilizante=new Producto(3,"fertilizante completo",800,0)
+const insecticida=new Producto(4,"insecticida",500,0)
+
+function agregarCarrito (){
+    while(seguirComprando===true){
+        choise=parseInt(prompt(`Elija un producto: ${maceta.id}. ${maceta.nombre},${sustrato.id}. ${sustrato.nombre}, ${fertilizante.id}. ${fertilizante.nombre},${insecticida.id}. ${insecticida.nombre}`))
+        if (choise===maceta.id){
+            carrito=carrito + maceta.nombre+" "
+            total= total+ maceta.precio
+        }else if (choise===sustrato.id){
+            carrito=carrito + sustrato.nombre+" "
+            total= total+ sustrato.precio
+        }else if (choise===fertilizante.id){
+            carrito=carrito + fertilizante.nombre+" "
+            total= total+ fertilizante.precio
+        }else if (choise===insecticida.id){
+            carrito=carrito + insecticida.nombre+" "
+            total= total+ insecticida.precio
+        }else{
+            choise=prompt("Por favor ingresa un producto correcto")
+        }
+        decision = parseInt(prompt("desea sieguir comprando 1.Si 2.No"))
+        if(decision===1){
+            choise = parseInt(prompt(`Elija un producto: ${maceta.id}. ${maceta.nombre},${sustrato.id}. ${sustrato.nombre}, ${fertilizante.id}. ${fertilizante.nombre},${insecticida.id}. ${insecticida.nombre}`))
+        }else if(decision===2){
+            seguirComprando=false
+        }else{
+            decision=parseInt(prompt("desea sieguir comprando 1.Si 2.No"))
+        }
+    }
+}
+function cuotas(){
+    modoDePago = parseInt(prompt(`tu total es ${total}, como deseas pagarlo 1. efectivo 10% de descuento-${total*0.9},2.en un pago por trasnferencia o tarjeta ${total} 3. 3 cuotas sin interes de ${total/3}, 4. 12 cuotas con interes de${(total*1.5)/12}.`))
+        if (modoDePago===1){
+            pagos = "un pago en efectivo de $ "+total*0.9
+        }else if (modoDePago===2){
+            pagos = "un pago de $ "+ total
+        }else if (modoDePago===3){
+            pagos = "3 cuotas de $ " +total/3
+        }else if (modoDePago===4){
+            pagos = "12 cuotas de $"+(total*1.5)/12
+        }else{
+            modoDePago=prompt("Por favor ingresa un metodo de pago correcto")
+            pago=true
+        }
+    alert(`Usted acaba de comprar ${carrito}, por un total de: $${total} su metodo de pago es:${pagos}`)
+}
+
+
+let carrito = " "
+let total =0
+let seguirComprando= true
+let pagos
+
+agregarCarrito()
+cuotas()
