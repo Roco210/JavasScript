d5c6870e9afe1ba812f160f10808b8dd96651283
Ricardo <rickyfernandez210@gmail.com>
2022-11-23 07:19:56 -0300

    version3

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 //creacion de clase
 class Producto {
-    constructor(id, nombre, precio, imagen, cantidad) {
+    constructor(id, nombre, precio, imagen, stock) {
         this.id = id,
-            this.nombre = nombre,
-            this.precio = precio,
-            this.imagen = imagen,
-            this.cantidad = cantidad
+        this.nombre = nombre,
+        this.precio = precio,
+        this.imagen = imagen,
+        this.stock = stock
     }
 }
+
+/* class Carrito{ 
+    constructor(){
+        this.id,
+        this.nombre=nombre,
+        this.cantidad=cantidad
+    }
+} */
 //creacion de objetos
-const maceta = new Producto(1, "maceta 5 lts", 1000, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKStZsebgqXi6Oxry4FdHiiqSNQg7KPrz4Jw&usqp=CAU", 1)
-const sustrato = new Producto(2, "sustrato ligero 25dm", 2200, " https://http2.mlstatic.com/D_NQ_NP_863283-MLA43259201302_082020-W.jpg", 1)
-const fertilizante = new Producto(3, "fertilizante completo", 800, " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnc021QXFS6JuYvhUzEJczjWKz4IlS4_p2zA&usqp=CAU", 1)
-const insecticida = new Producto(4, "insecticida", 500, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAljp23Kld9LQa4HaeUC68qHSIBOtrrY_-jw&usqp=CAU ", 1)
+const maceta = new Producto(1, "maceta 5 lts", 1000, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKStZsebgqXi6Oxry4FdHiiqSNQg7KPrz4Jw&usqp=CAU", 10)
+const sustrato = new Producto(2, "sustrato ligero 25dm", 2200, " https://http2.mlstatic.com/D_NQ_NP_863283-MLA43259201302_082020-W.jpg", 10)
+const fertilizante = new Producto(3, "fertilizante completo", 800, " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnc021QXFS6JuYvhUzEJczjWKz4IlS4_p2zA&usqp=CAU", 10)
+const insecticida = new Producto(4, "insecticida", 500, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAljp23Kld9LQa4HaeUC68qHSIBOtrrY_-jw&usqp=CAU ", 10)
 // array de objetos
 
 let stock = []
@@ -20,11 +28,12 @@ stock.push(maceta)
 stock.push(sustrato)
 stock.push(fertilizante)
 stock.push(insecticida)
-
+localStorage.setItem("base",JSON.stringify(stock))
 // interaccion con el DOM principal
 
+let infobase =JSON.parse(localStorage.getItem("base"))
 let card = document.getElementById("producto")
-stock.forEach(p => {
+infobase.forEach(p => {
     card.innerHTML += `
 <div class="card" style="width: 18rem;">
     <div class="card-body">
@@ -39,23 +48,46 @@ stock.forEach(p => {
 let a = 0
 let b
 let carrito = []
+let elementos =[]
 let finalizar = document.getElementById("finalizar")
 let cuerpo =document.getElementById("tarjeta")
 let body=document.getElementById("cuerpo")
+localStorage.setItem("carrito",JSON.stringify(elementos))
 //selecciona productos y los guarda en el carrito, y el tottal de lo comprado
 function d(){
     card.onclick=(e)=>{
         let ip=parseInt((e.target.id))
         let productoEscojido = stock.find(element=>element.id===ip)
-        carrito.push(productoEscojido)
+        let temp =carrito.includes(productoEscojido)
+        if(temp){
+            let elementos = JSON.parse(localStorage.getItem("carrito"))
+            let temp =elementos.find(e=>e.id===ip)
+            temp.cantidad=temp.cantidad+1;
+            localStorage.setItem("carrito",JSON.stringify(elementos))
+            console.log(elementos)
+            console.log(temp)
+
+        }else{  
+            carrito.push(productoEscojido)
+            localStorage.getItem("carrito",JSON.stringify(elementos))
+            const elemento =  {"id":ip,"nombre":productoEscojido.nombre,"cantidad":1}
+            elementos.push(elemento)
+            
+            localStorage.setItem("carrito",JSON.stringify(elementos))
+            console.log("chau")
+        }
+    
+
+        localStorage.setItem(`${productoEscojido.nombre}`,`${productoEscojido.nombre}`)
         a += productoEscojido.precio
         localStorage.setItem("total", a)
     }
-
 }
+
 //cambia de pantalla a una ventana con los items elegidos y total de compra
 function cerrar (){
     finalizar.onclick = () => {
+        console.log (carrito)
         cuerpo.remove()
         a=localStorage.getItem("total")
         console.log(a)
